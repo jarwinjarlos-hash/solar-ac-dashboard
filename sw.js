1f424e15e4cb979e409e43d1a0b54d2ffb401cfc
jarwinjarlos-hash <jarwin.jarlos@gmail.com>
2026-07-14 09:30:05 +0800

    Implement service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'solar-ac-v2';
+const ASSETS = [
+  'index.html',
+  'app.js',
+  'manifest.json'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
